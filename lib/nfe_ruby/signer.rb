ad7ef0816ae8c28946d47346ca1a9fc0f77a57a6
Tarcisiosk <tarcisioskora@hotmail.com>
2015-11-09 11:11:19 -0200

    Update signer.rb
    PKEY CERTIFICATE

--- lib/nfe_ruby/signer.rb
+++ lib/nfe_ruby/signer.rb
@@ -2,8 +2,10 @@ module NfeRuby
   class Signer
     def initialize(options = {})
       # Inicializar certificado, chave privada e digest
-      @pub_key = OpenSSL::X509::Certificate.new(File.read(options[:cert_file])) if options[:cert_file].present?
-      @priv_key = OpenSSL::PKey::RSA.new(File.read(options[:priv_key_file]), options[:priv_key_pass]) if options[:priv_key_file].present?
+      @pub_key = :cert_file
+      #OpenSSL::X509::Certificate.new(File.read(options[:cert_file])) if options[:cert_file].present?
+      @priv_key = :priv_key_file
+      #OpenSSL::PKey::RSA.new(File.read(options[:priv_key_file]), options[:priv_key_pass]) if options[:priv_key_file].present?
       @digest = OpenSSL::Digest::SHA1.new
     end
 
